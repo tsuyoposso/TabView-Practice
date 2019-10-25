b3eac7de21311b363c8b32499e8dce768d7d5905
tsuyoposso <51411162+tsuyoposso@users.noreply.github.com>
2019-10-25 15:33:28 +0900

    frist commit

---
+++ Estimation/Estimation/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Estimation
+//
+//  Created by 長坂豪士 on 2019/10/25.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Estimation/Estimation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Estimation
+//
+//  Created by 長坂豪士 on 2019/10/25.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
